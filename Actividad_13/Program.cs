1cbd0dd01dc315e9874a284e8e757ded81125b1b
MARIA RENEÉ ROJAS ARCAYNE <88RO94097790@campus.economicas.uba.ar>
2021-03-27 12:47:44 -0300

    Entrega de la actividad 13, a pedido del docente.

---
+++ Actividad_13/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_13
+{
+    class Program
+    {
+        //Solicite el ingreso de dos frases y determine si tienen la misma longitud.
+        static void Main(string[] args)
+        {
+            string Frase_1;
+            string Frase_2;
+
+            Console.Write("Por favor, ingrese la primera frase: ");
+            Frase_1 = Console.ReadLine();
+            Console.Write("Por favor, ingrese la segunda frase: ");
+            Frase_2 = Console.ReadLine();
+            Console.WriteLine("\r");
+
+            if (Frase_1.Length == Frase_2.Length)
+            {
+                Console.WriteLine("Las frases ingresadas tienen la misma longitud de largo.");
+            }
+            else
+            {
+                Console.WriteLine("Las frases ingresadas no tienen la misma longitud de largo.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
